4dc2fe30fc093a581da6969fc3eac2af9c565aec
Pantawat <pantawat_b@cmu.ac.th>
2023-07-22 18:42:00 +0700

    wdv

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -8,6 +8,9 @@ export const Task = ({
   const deleteBtnOnClick = () => {
     deleteTaskFunc(id);
   };
+  const doneBtnOnClick = () => {
+    toggleDoneTaskFunc(id);
+  };
 
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
@@ -17,8 +20,14 @@ export const Task = ({
         But if task is not completed : 
         <span>{title}</span>
       */}
-      <span>{title}</span>
-      <button className="btn btn-success">Done</button>
+      {completed ? (
+        <span className="text-decoration-line-through">{title}</span>
+      ) : (
+        <span>{title}</span>
+      )}
+      <button className="btn btn-success" onClick={doneBtnOnClick}>
+        Done
+      </button>
       <button className="btn btn-danger" onClick={deleteBtnOnClick}>
         Delete
       </button>
